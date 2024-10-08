2be8607f6531a9519d785530725add3f24750e44
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2024-10-08 16:04:05 -0400

    up to step 10 done

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
-// app.js
 const express = require("express");
-const path = require("node:path");
+const path = require("path");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-const links = [
-  { href: "/", text: "Home" },
-  { href: "about", text: "About" },
+// Example messages for the assignment
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
 ];
 
-const users = ["Rose", "Cake", "Biff"];
-
-const assetsPath = path.join(__dirname, "public");
-
-app.use(express.static(assetsPath));
-
-app.get("/", (req, res) => {
-  res.render("index", { links: links, users: users });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about", { links: links, message: "It's About time." });
-});
-
-app.set("views", path.join(__dirname, "views"));
+// Set the view engine to EJS
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // Set the views directory
 
+// Serve static files like CSS
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route for the homepage
 app.get("/", (req, res) => {
-  res.render("index", { message: "EJS rocks!" });
+  res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
-app.listen(3000, function () {
-  console.log(`App listening on port 3000!`);
+// Using this port number:
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// New message form
+app.get("/new", (req, res) => {
+  res.render("form"); 
+});
+
+app.post("/new", (req, res) => {
+  let currentUser = req.body.currentUser;
+  let currentMessage = req.body.currentMessage;
+  messages.push({ text: currentMessage, user: currentUser, added: new Date() });
+  res.redirect("/");
 });
