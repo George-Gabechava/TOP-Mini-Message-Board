50519db8d4382d1d514d88731879f590fa1b5319
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2025-02-06 09:09:48 -0500

    logging env variable to debug

--- db/pool.js
+++ db/pool.js
@@ -1,13 +1,10 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-// All of the following properties should be read from environment variables
+// Debugging: Log environment variable
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
-  // host: process.env.DB_HOST, 
-  // user: process.env.DB_USER,
-  // database: process.env.DB_NAME,
-  // password: process.env.DB_PASSWORD,
-  // port: process.env.DB_PORT
 });
