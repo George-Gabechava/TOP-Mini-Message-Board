e7514cd7c00771c775aad54fc2581697a6a8eabf
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2025-02-07 11:05:38 -0500

    removed text decorations

---
+++ db/populatedb.js
@@ -0,0 +1,14 @@
+// populatedb.js
+// Example messages for the assignment
+const messages = [
+  {
+    text: "Hello, please be nice on my website :)",
+    name: "George",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    name: "Test",
+    added: new Date()
+  }
+];
